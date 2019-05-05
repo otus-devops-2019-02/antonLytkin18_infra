2513c9fe1eedfc48f6fe9a4c7a8dff7e3d3c7fbc
Anton Lytkin <a.lytkin@worksolutions.ru>
2019-05-05 21:22:32 +0300

    Add host name map.

--- ansible/inventory.py
+++ ansible/inventory.py
@@ -7,12 +7,21 @@ import os
 
 class InventoryGenerator:
 
+    terraform_output_map = {
+        'app_external_ip': 'app',
+        'db_external_ip': 'db'
+    }
+
     def __init__(self):
         self._hosts = {}
         os.chdir('../terraform/stage/')
         terraform_result = subprocess.check_output(['terraform', 'output', '-json'])
         for key, item in json.loads(terraform_result).iteritems():
             self._hosts[key] = item['value']
+
+    def get_host_name(self, terraform_name):
+        host_name = self.terraform_output_map[terraform_name]
+        return host_name if host_name else terraform_name + '_host'
 
     def print_json(self):
         hostvars = {}
@@ -28,9 +37,9 @@ class InventoryGenerator:
 
         for name, ip_address in self._hosts.iteritems():
             hostvars[name] = {'ansible_host': ip_address}
-            server_name = name + '_server'
-            all.append(server_name)
-            inventory[server_name] = {'hosts': [name]}
+            host_name = self.get_host_name(name)
+            all.append(host_name)
+            inventory[host_name] = {'hosts': [name]}
 
         print(json.dumps(inventory))
 
